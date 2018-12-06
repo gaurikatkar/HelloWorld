79e518a95710568fd0d02030ced84deb67560b35
gaurikatkar <gaurikatkar20@gmail.com>
2018-12-06 14:58:18 +0530

    New class of demo for demo module

---
+++ src/HelloWord1/DemoofDemo.java
@@ -0,0 +1,14 @@
+package HelloWord1;
+
+public class DemoofDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				System.out.println("Hiiiiii");
+	}
+
+	public void m1()
+	{
+		
+	}
+}
